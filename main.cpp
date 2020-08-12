446c49d3f6f147340b14ba403d1664fe825e8112
Igor <igorigorenko00@gmail.com>
2020-08-12 17:23:04 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,12 +11,12 @@
 double monteCarlo(long int count){
   int rezult = 0;
   double x,y;
-  long int i = 0;
+  
 
   time_t timer;
   time(&timer);
 
-  for(; i < count; i++){
+  for(long int i = 0; i < count; i++){
       x = (double)rand()/RAND_MAX;
       y = (double)rand()/RAND_MAX;
       if (x * x + y * y <= 1)
@@ -44,7 +44,7 @@ void* thread_calc(void* args){
 void* load(void* args){
   int loader = 0;
   while(1)
-  loader*= 3;
+    loader*= 3;
 }
 
 int main(int argc, char* argv[]){
@@ -53,43 +53,39 @@ int main(int argc, char* argv[]){
      printf("Wrong Input!");
      return 0;
   }
-  int thread_number = strtol(argv[1], NULL, 10);
+
+  int thread_num = strtol(argv[1], NULL, 10);
   int cpu_number = get_nprocs();
   double answer = 0;
-
   cpu_set_t cpu_set;
-  thread_args_str* thread_args_array = (thread_args_str*)calloc(thread_number, sizeof(thread_args_str));
+  thread_args_str* thread_args_arr = (thread_args_str*)calloc(thread_num, sizeof(thread_args_str));
 
-
-  for(int i = thread_number; i < cpu_number; ++i){
+  for(int i = thread_num; i < cpu_number; i++){
     CPU_ZERO(&cpu_set);
     CPU_SET(i, &cpu_set);
-
-    thread_args_array[i].pthread = pthread_self();
-    pthread_setaffinity_np(thread_args_array[i].pthread, sizeof(cpu_set_t), &cpu_set);
-    pthread_create(&(thread_args_array[i].pthread), NULL, load, NULL);
+    thread_args_arr[i].pthread = pthread_self();
+    pthread_setaffinity_np(thread_args_arr[i].pthread, sizeof(cpu_set_t), &cpu_set);
+    pthread_create(&(thread_args_arr[i].pthread), NULL, load, NULL);
   }
 
   srand( time(NULL) );
-  for(int i = 0; i < thread_number; ++i)
-  {
-    thread_args_array[i].left_end = LEFT_END+(RIGHT_END-LEFT_END)/thread_number*i;
-    thread_args_array[i].right_end = LEFT_END+(RIGHT_END-LEFT_END)/thread_number*(i+1);
-    thread_args_array[i].result = 0;
-    thread_args_array[i].pthread = pthread_self();
 
+  for(int i = 0; i < thread_num; i++){
     CPU_ZERO(&cpu_set);
     CPU_SET(i%cpu_number ,&cpu_set);
-
-    pthread_setaffinity_np(thread_args_array[i].pthread, sizeof(cpu_set_t), &cpu_set);
-    pthread_create(&(thread_args_array[i].pthread), NULL, thread_calc, (void*)&thread_args_array[i]);
+    thread_args_arr[i].result = 0;
+    thread_args_arr[i].left_end = LEFT_END+(RIGHT_END-LEFT_END)/thread_num*i;
+    thread_args_arr[i].right_end = LEFT_END+(RIGHT_END-LEFT_END)/thread_num*(i+1);
+    thread_args_arr[i].pthread = pthread_self();
+    pthread_setaffinity_np(thread_args_arr[i].pthread, sizeof(cpu_set_t), &cpu_set);
+    pthread_create(&(thread_args_arr[i].pthread), NULL, thread_calc, (void*)&thread_args_arr[i]);
   }
-  for (int i = 0; i < thread_number; ++i)
-    pthread_join(thread_args_array[i].pthread, NULL);
+  for (int i = 0; i < thread_num; i++)
+    pthread_join(thread_args_arr[i].pthread, NULL);
 
-  for (int i = 0; i < thread_number; ++i){
-    answer += thread_args_array[i].result;
-//    printf("%d - %f\n", i, thread_args_array[i].result );
+  for (int i = 0; i < thread_num; i++){
+    answer += thread_args_arr[i].result;
+//    printf("%d - %f\n", i, thread_args_arr[i].result );
   }
-    printf("%f\n", 4 * answer / thread_number);
+    printf("%f\n", 4 * answer / thread_num);
 }
